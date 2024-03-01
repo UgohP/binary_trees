9c49955f111358e143f0bfc16e599095f4eac1f4
Paschal Ugoh <paskyugoh@gmail.com>
2024-02-29 20:14:42 -0800

    changes in prototype

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,7 +8,7 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new node.
  */
-binary_tree_t *binary_tree_node0(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
--- binary_trees.h
+++ binary_trees.h
@@ -42,7 +42,7 @@ typedef struct levelorder_queue_s
 void binary_tree_print(const binary_tree_t *);
 
 /* Tasks Function Prototypes */
-binary_tree_t *binary_tree_node0(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
 
