4b6d16b8107eda812aac0075bf705a6d5f2cc050
Paschal Ugoh <paskyugoh@gmail.com>
2024-03-01 20:36:09 -0800

    fixed error at .h file

--- binary_trees.h
+++ binary_trees.h
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -43,17 +42,12 @@ void binary_tree_print(const binary_tree_t *);
 
 /* Tasks Function Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-void binary_tree_delete(binary_tree_t *tree)
-int binary_tree_is_leaf(const binary_tree_t *node)
-int binary_tree_is_root(const binary_tree_t *node)
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
-
-
-
-
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _BINARY_TREES_H_ */
